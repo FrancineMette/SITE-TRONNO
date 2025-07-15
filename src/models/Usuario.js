14dcfd1bab3ee7fbe2aeba6c56d08b810d899f2d
FrancineMette <francineisabelsantos@hotmail.com>
2025-07-15 09:34:15 -0300

    usuario updates

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -2,13 +2,28 @@ const db = require('../config/db');
 
 const Usuario = {
   criar: (usuario, callback) => {
-    const sql = 'INSERT INTO usuarios (nome_completo, nome_usuario, email, senha) VALUES (?, ?, ?, ?)';
-    db.query(sql, [usuario.nome, usuario.usuario, usuario.email, usuario.senha], callback);
+    const sql = 'INSERT INTO usuarios (nome_completo, usuario, email, senha, criado_em) VALUES (?, ?, ?, ?, NOW())';
+    
+    db.query(sql, [usuario.nome, usuario.usuario, usuario.email, usuario.senha], (err, result) => {
+      if (err) {
+        console.error('❌ Erro ao inserir usuário:', err);
+        return callback(err);
+      }
+      console.log('✅ Usuário inserido com sucesso!', result);
+      callback(null, result);
+    });
   },
 
   buscarPorEmailOuUsuario: (login, callback) => {
-    const sql = 'SELECT * FROM usuarios WHERE email = ? OR nome_usuario = ?';
-    db.query(sql, [login, login], callback);
+    const sql = 'SELECT * FROM usuarios WHERE email = ? OR usuario = ?';
+    
+    db.query(sql, [login, login], (err, results) => {
+      if (err) {
+        console.error('❌ Erro ao buscar usuário:', err);
+        return callback(err);
+      }
+      callback(null, results);
+    });
   }
 };
 
