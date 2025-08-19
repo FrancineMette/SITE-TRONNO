54d0d4b2a8b91f02c038177720196e085e2d3d8d
FrancineMette <francineisabelsantos@hotmail.com>
2025-08-19 11:10:38 -0300

    teste debug

--- src/routes/colaboradoresRoutes.js
+++ src/routes/colaboradoresRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const { autenticarToken, requireAdmin } = require('../../middleware/auth');
-const controller = require('../controllers/colaboradorController');
+const colaboradorController = require('../controllers/colaboradorController');
 
 router.post('/', colaboradorController.criar);
 router.post('/login', colaboradorController.login);
